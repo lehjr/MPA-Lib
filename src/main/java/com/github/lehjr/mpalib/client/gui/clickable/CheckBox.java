5f5aa4d754521df1f19d302b181dbed9de114a4a
lehjr <lehjr1@gmail.com>
2020-02-11 11:37:27 -0500

    add checkbox

---
+++ src/main/java/com/github/lehjr/mpalib/client/gui/clickable/CheckBox.java
@@ -0,0 +1,134 @@
+package com.github.lehjr.mpalib.client.gui.clickable;
+
+import com.github.lehjr.mpalib.client.gui.geometry.DrawableTile;
+import com.github.lehjr.mpalib.client.gui.geometry.Point2D;
+import com.github.lehjr.mpalib.client.render.Renderer;
+import com.github.lehjr.mpalib.client.sound.Musique;
+import com.github.lehjr.mpalib.math.Colour;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.ITextComponent;
+
+import java.util.List;
+
+public class CheckBox implements IClickable{
+    protected IPressable onPressed;
+    protected IReleasable onReleased;
+
+    protected boolean isVisible;
+    protected boolean isEnabled;
+
+    protected boolean isChecked;
+    protected DrawableTile tile;
+
+    String label;
+    final int id;
+
+    public CheckBox(int id, Point2D position, String displayString, boolean isChecked){
+        this.id = id;
+        Point2D ul = position.plus(4, 4);
+        this.tile = new DrawableTile(ul, ul.plus(8, 8), Colour.BLACK, Colour.DARKGREY);
+        this.label = displayString;
+        this.isChecked = isChecked;
+        this.enableAndShow();
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        if(this.isVisible) {
+            tile.draw();
+            if (isChecked) {
+                Renderer.drawString("x", tile.centerx() - 2, tile.centery() - 5, Colour.WHITE);
+            }
+            Renderer.drawString(label, tile.centerx() + 8, tile.centery() - 4, Colour.WHITE);
+        }
+    }
+
+    @Override
+    public boolean hitBox(double x, double y) {
+        if (this.isVisible() && this.isEnabled()) {
+            return x >= this.tile.left() && x <= this.tile.right() && y >= this.tile.top() && y <= this.tile.bottom();
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void move(double x, double y) {
+        this.setPosition(getPosition().plus(x, y));
+    }
+
+    @Override
+    public Point2D getPosition() {
+        return null;
+    }
+
+    @Override
+    public void setPosition(Point2D position) {
+        Point2D ul = position.plus(4, 4);
+        tile.setLeft(ul.getX());
+        tile.setTop(ul.getY());
+    }
+
+    @Override
+    public List<ITextComponent> getToolTip() {
+        return null;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.isChecked = checked;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        this.isEnabled = enabled;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    @Override
+    public void setVisible(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+
+    @Override
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    @Override
+    public void setOnPressed(IPressable iPressable) {
+        this.onPressed = iPressable;
+    }
+
+    @Override
+    public void setOnReleased(IReleasable iReleasable) {
+        this.onReleased = iReleasable;
+    }
+
+    @Override
+    public void onReleased() {
+        if (this.isVisible && this.isEnabled && this.onReleased != null) {
+            this.onReleased.onReleased(this);
+        }
+    }
+
+    @Override
+    public void onPressed() {
+        if (this.isVisible && this.isEnabled) {
+            Musique.playClientSound(SoundEvents.UI_BUTTON_CLICK, 1);
+            this.isChecked = !this.isChecked;
+            if (this.onPressed != null) {
+                this.onPressed.onPressed(this);
+            }
+        }
+    }
+}
